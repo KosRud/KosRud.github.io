3eae5666b03f990f8ebf97aff4e18d9b8b8f492b
Kostiantyn <hidden@none.com>
2022-03-26 21:20:58 +0100

    wip seminar 6

--- docs/For students (JS)/Seminar-examples/Seminar-6/MouseDragDemo/GameObject/GameObjectModel.js
+++ docs/For students (JS)/Seminar-examples/Seminar-6/MouseDragDemo/GameObject/GameObjectModel.js
@@ -1,7 +1,7 @@
- import Vector2 from "../Utility/Vector2.js"
+import Vector2 from '../Utility/Vector2.js';
 
- export default class GameObjectModel {
-     constructor() {
-         this.position = new Vector2();
-     }
- }
+export default class GameObjectModel {
+    constructor() {
+        this.position = new Vector2();
+    }
+}
--- docs/For students (JS)/Seminar-examples/Seminar-6/MouseDragDemo/GameObject/Gem/GemView.js
+++ docs/For students (JS)/Seminar-examples/Seminar-6/MouseDragDemo/GameObject/Gem/GemView.js
@@ -1,19 +1,31 @@
-import View from "../../MVC/View.js";
+import View from '../../MVC/View.js';
 
 export default class GemView extends View {
     draw(canvas) {
         const ctx = canvas.getContext('2d');
 
+        let strokeStyle = 'black';
+        switch (true) {
+            case this.model.draggingOffset != null:
+                strokeStyle = 'yellow';
+                break;
+            case this.model.isMouseOver:
+                strokeStyle = 'blue';
+                break;
+        }
+
         ctx.beginPath();
-        ctx.save(); {
+        ctx.save();
+        {
             /*
                 this { } block simply emphasizes save-restore
                 it is here only to improve readability
             */
 
             ctx.fillStyle = this.model.color;
-            ctx.strokeStyle = "black";
-            ctx.arc( // circle
+            ctx.strokeStyle = strokeStyle;
+            ctx.arc(
+                // circle
                 this.model.position.x,
                 canvas.height - this.model.position.y, // flip y coordinate
                 this.model.size, // radius
