46aca0b5c985dab3cb15e671b81901455e440fe5
Kostiantyn <hidden@none.com>
2022-03-27 00:52:51 +0100

    refactor

--- docs/For students (JS)/Seminar-examples/Seminar-5/Game/Game.js
+++ docs/For students (JS)/Seminar-examples/Seminar-5/Game/Game.js
@@ -1,134 +1,101 @@
-import PlayerController from "./Character/Player/PlayerController.js";
-import PlayerView from "./Character/Player/PlayerView.js";
-import PlayerModel from "./Character/Player/PlayerModel.js";
+import PlayerController from './Character/Player/PlayerController.js';
+import PlayerView from './Character/Player/PlayerView.js';
+import PlayerModel from './Character/Player/PlayerModel.js';
 
-import ThiefController from "./Character/Enemy/Thief/ThiefController.js";
-import ThiefView from "./Character/Enemy/Thief/ThiefView.js";
-import ThiefModel from "./Character/Enemy/Thief/ThiefModel.js";
+import ThiefController from './Character/Enemy/Thief/ThiefController.js';
+import ThiefView from './Character/Enemy/Thief/ThiefView.js';
+import ThiefModel from './Character/Enemy/Thief/ThiefModel.js';
 
-import SlimeController from "./Character/Enemy/Slime/SlimeController.js";
-import SlimeView from "./Character/Enemy/Slime/SlimeView.js";
-import SlimeModel from "./Character/Enemy/Slime/SlimeModel.js";
+import SlimeController from './Character/Enemy/Slime/SlimeController.js';
+import SlimeView from './Character/Enemy/Slime/SlimeView.js';
+import SlimeModel from './Character/Enemy/Slime/SlimeModel.js';
 
-import SkeletonController from "./Character/Enemy/Skeleton/SkeletonController.js";
-import SkeletonView from "./Character/Enemy/Skeleton/SkeletonView.js";
-import SkeletonModel from "./Character/Enemy/Skeleton/SkeletonModel.js";
+import SkeletonController from './Character/Enemy/Skeleton/SkeletonController.js';
+import SkeletonView from './Character/Enemy/Skeleton/SkeletonView.js';
+import SkeletonModel from './Character/Enemy/Skeleton/SkeletonModel.js';
 
-import Vector2 from "./Utility/Vector2.js";
+import Vector2 from './Utility/Vector2.js';
 
-import {
-    InputManager
-} from "./Utility/InputManager.js";
+import { InputManager } from './Utility/InputManager.js';
 
 export default class Game {
-    constructor({
-        logDiv,
-        canvas,
-        gameOver,
-        victory
-    }) {
+    constructor({ logDiv, canvas, gameOver, victory }) {
         this.enemies = [];
         this.canvas = canvas;
         this.logDiv = logDiv;
-        this.gameOver = gameOver
+        this.gameOver = gameOver;
         this.victory = victory;
 
         this._isRunning = false;
 
+        this.inputManager = InputManager;
+
+        this._initPlayer();
+        this._initEnemies();
+    }
+
+    _initEnemies() {
         const canvasCenter = new Vector2(
             this.canvas.width / 2,
             this.canvas.height / 2
         );
 
-        this.inputManager = InputManager;
+        // add slime
 
-        // init player
-        {
-            /*
-                These braces are not necessary,
-                but they provide isolation.
+        let slimeModel = new SlimeModel(canvasCenter);
+        let slimeView = new SlimeView(slimeModel);
+        this.enemies.push(
+            new SlimeController({
+                model: slimeModel,
+                view: slimeView,
+                game: this,
+            })
+        );
 
-                Variables created with "let" will not be accessible
-                outside the block, where they were created.
+        // add skeleton
 
-                The less variables you have to think about - the better.
-                Easier to understand what your code is doing, and find bugs.
+        let skeletonModel = new SkeletonModel(canvasCenter);
+        let skeletonView = new SkeletonView(skeletonModel);
+        this.enemies.push(
+            new SkeletonController({
+                model: skeletonModel,
+                view: skeletonView,
+                game: this,
+            })
+        );
 
-                This is one of the reasons to prefer "let" over "var"!
+        // add thief
 
-                We could also create a function "initPlayer()" inside Game
-            */
+        let thiefModel = new ThiefModel(canvasCenter);
+        let thiefView = new ThiefView(thiefModel);
+        this.enemies.push(
+            new ThiefController({
+                model: thiefModel,
+                view: thiefView,
+                game: this,
+            })
+        );
+    }
 
-            let playerModel = new PlayerModel({
-                name: "Player",
-                position: new Vector2(0, 0)
-            });
+    _initPlayer() {
+        let playerModel = new PlayerModel({
+            name: 'Player',
+            position: new Vector2(0, 0),
+        });
 
-            // let the player start in a corner
-            playerModel.position = new Vector2(
-                playerModel.size,
-                playerModel.size
-            ).add(
-                new Vector2(
-                    20,
-                    20
-                )
-            );
+        // let the player start in a corner
+        playerModel.position = new Vector2(
+            playerModel.size,
+            playerModel.size
+        ).add(new Vector2(20, 20));
 
-            let playerView = new PlayerView(playerModel);
+        let playerView = new PlayerView(playerModel);
 
-            this.playerController = new PlayerController({
-                model: playerModel,
-                view: playerView,
-                game: this
-            });
-        }
-
-        //  Game.log(playerModel); - this would produce an error
-
-        // init enemies
-
-        {
-            // add slime
-
-            let slimeModel = new SlimeModel(canvasCenter);
-            let slimeView = new SlimeView(slimeModel);
-            this.enemies.push(
-                new SlimeController({
-                    model: slimeModel,
-                    view: slimeView,
-                    game: this
-                })
-            );
-        }
-
-        {
-            // add skeleton
-
-            let skeletonModel = new SkeletonModel(canvasCenter);
-            let skeletonView = new SkeletonView(skeletonModel);
-            this.enemies.push(
-                new SkeletonController({
-                    model: skeletonModel,
-                    view: skeletonView,
-                    game: this
-                })
-            );
-        }
-
-        {
-            // add thief
-
-            let thiefModel = new ThiefModel(canvasCenter);
-            let thiefView = new ThiefView(thiefModel);
-            this.enemies.push(
-                new ThiefController({
-                    model: thiefModel,
-                    view: thiefView,
-                    game: this
-                })
-            );
-        }
+        this.playerController = new PlayerController({
+            model: playerModel,
+            view: playerView,
+            game: this,
+        });
     }
 
     log(str, ...classList) {
@@ -153,7 +120,7 @@ export default class Game {
                 https://constantinerudenko.github.io/For%20students%20%28JS%29/JS%20tricks/#spread-syntax
         */
 
-        let logLine = document.createElement("p");
+        let logLine = document.createElement('p');
         logLine.innerHTML = str;
         for (const htmlClass of classList) {
             logLine.classList.add(htmlClass);
@@ -161,7 +128,7 @@ export default class Game {
         this.logDiv.append(logLine);
 
         this.logDiv.scrollTo({
-            top: this.logDiv.scrollHeight
+            top: this.logDiv.scrollHeight,
         });
     }
 
@@ -172,11 +139,9 @@ export default class Game {
             enemyController.update(this);
         }
 
-        this.enemies = this.enemies.filter(
-            (enemyController) => {
-                return enemyController.model.isDead == false
-            }
-        );
+        this.enemies = this.enemies.filter((enemyController) => {
+            return enemyController.model.isDead == false;
+        });
     }
 
     _draw() {
