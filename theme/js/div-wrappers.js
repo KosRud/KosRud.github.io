8365298df4bd2b7e66a05f730303ae81d340dd48
MadCake <->
2022-09-25 09:02:12 +0200

    edit css

--- theme/js/div-wrappers.js
+++ theme/js/div-wrappers.js
@@ -6,4 +6,8 @@ window.addEventListener("load", () => {
     document.querySelectorAll("figure").forEach((figure) => {
         figure.innerHTML = `<div>${figure.innerHTML}</div>`;
     });
+
+    document.querySelectorAll(".admonition").forEach((admonition) => {
+        admonition.outerHTML = `<div class="admonition-wrapper">${admonition.outerHTML}</div>`;
+    });
 });
