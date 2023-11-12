67ac4d465eb4777669f3a588bf4be631704ae0a7
MadCake <placeholder@email.com>
2023-11-12 11:33:18 +0100

    wip homepage

--- pages/components/HomePage.vue
+++ pages/components/HomePage.vue
@@ -7,32 +7,48 @@ const highlights = [
 </script>
 
 <template>
-    <section :class="$style.Hero">
-        <div :class="$style.SiteNameContainer">
-            <h2 :class="$style.SiteNameContainer_title">Kostiantyn Rudenko</h2>
-            <h3 :class="$style.SiteNameContainer_subtitle">personal website</h3>
+    <div :class="$style.HomePage">
+        <section :class="$style.Hero">
+            <div :class="$style.SiteNameContainer">
+                <h2 :class="$style.SiteNameContainer_title">
+                    Kostiantyn Rudenko
+                </h2>
+                <h3 :class="$style.SiteNameContainer_subtitle">
+                    personal website
+                </h3>
+            </div>
+        </section>
+        <div :class="$style.ContentWrapper">
+            <section :class="$style.ContentWrapper_content">
+                <section :class="$style.CardGallery">
+                    <article
+                        :class="$style.CardGallery_card"
+                        v-for="highlight in highlights"
+                    >
+                        {{ highlight.name }}
+                    </article>
+                    <article
+                        :class="[
+                            $style.CardGallery_card,
+                            $style.CardGallery_card___dummy,
+                        ]"
+                        v-for="_ in Array.from({ length: 16 })"
+                    ></article>
+                </section>
+            </section>
         </div>
-    </section>
-    <section :class="$style.CardGallery">
-        <article
-            :class="$style.CardGallery_card"
-            v-for="highlight in highlights"
-        >
-            {{ highlight.name }}
-        </article>
-        <article
-            :class="[$style.CardGallery_card, $style.CardGallery_card___dummy]"
-            v-for="_ in Array.from({ length: 16 })"
-        ></article>
-    </section>
+    </div>
 </template>
 
 <style module lang="less">
 @import "../../.vitepress/theme/style/main/index.less";
 
+.HomePage {
+    height: 100%;
+}
+
 .Hero {
-    height: 500rem;
-    background-color: @color-primary;
+    background-color: @color-primary-muted;
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -47,20 +63,38 @@ const highlights = [
     flex-direction: column;
     align-items: end;
     justify-content: center;
+
+    text-shadow: 2px 2px 0px black, 4px 4px 12px #000a;
+    line-height: 1.2;
+    margin: @gap*8;
 }
 
 .SiteNameContainer_title {
+    font-weight: bold;
     font-size: @size-hero;
 }
 
 .SiteNameContainer_subtitle {
     font-size: @size-hero-s;
 }
+
+.ContentWrapper {
+    padding: @gap*2;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    box-shadow: @shadow-inverted;
+}
+
+.ContentWrapper_content {
+    max-width: 1200rem;
+}
 .CardGallery {
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
-    gap: @gap;
+    gap: @gap*4;
     justify-content: stretch;
     align-items: start;
     width: 100%;
