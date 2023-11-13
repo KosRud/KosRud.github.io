82b5805f3668d024d494c299f736b5ae834706a8
MadCake <placeholder@email.com>
2023-11-14 00:35:05 +0100

    wip homepage

---
+++ pages/components/FeaturesGalleryCard.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+const props = defineProps<{
+    dummy?: boolean;
+    name: string;
+}>();
+</script>
+
+<template>
+    <article
+        :class="[
+            $style.Card,
+            props.dummy ? $style.CardGallery_card___dummy : '',
+        ]"
+    >
+        <template v-if="!props.dummy">
+            <h2 :class="$style.Feature_title">{{ props.name }}</h2>
+            <slot />
+        </template>
+    </article>
+</template>
+
+<style module lang="less">
+@import "../../.vitepress/theme/style/main/index.less";
+
+.Card {
+    flex: 1 0 300rem;
+    min-height: 160rem;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: stretch;
+    align-items: stretch;
+
+    background: @color-gray-light;
+    box-shadow: @shadow;
+    border: 1rem solid @color-gray;
+    padding: @gap @gap*2;
+}
+
+.CardGallery_card___dummy {
+    height: 0rem;
+    min-height: 0rem;
+
+    border: none;
+    padding: 0rem 0rem;
+}
+
+.Feature_title {
+    font-size: @size-l;
+    // font-family: @font-compact;
+    font-weight: @weight-bold;
+    text-align: center;
+
+    margin-bottom: @gap;
+}
+</style>
