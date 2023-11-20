2eb41d33a51bdf07677c89a9dd4c32e580412939
MadCake <placeholder@email.com>
2023-11-20 11:53:56 +0100

    YouAreHere.vue

---
+++ .vitepress/theme/YouAreHere.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { useRoute } from "vitepress";
+const route = useRoute();
+</script>
+
+<template>
+    <!--  -->
+    <strong>You are here:</strong> {{ route.path }}
+</template>
