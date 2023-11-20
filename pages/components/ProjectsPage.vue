03cf725388ef66757796402b570d6d68501baa4c
MadCake <placeholder@email.com>
2023-11-20 07:29:31 +0100

    removed unused css

--- pages/components/ProjectsPage.vue
+++ pages/components/ProjectsPage.vue
@@ -60,30 +60,3 @@ import ProjectsPageProject from "./ProjectsPageProject.vue";
         <p>...</p>
     </ProjectsPageProject>
 </template>
-
-<style module lang="less">
-@import "../../.vitepress/theme/style/variables/index.less";
-
-.Project_title {
-    display: flex;
-    gap: @gap*2;
-    align-items: center;
-    justify-content: space-between;
-    flex-wrap: wrap;
-}
-
-.Project_devIcons {
-    display: flex;
-    gap: @gap*0.5;
-}
-
-.DevIcon {
-    width: @size*2;
-    aspect-ratio: 1;
-    vertical-align: bottom;
-    background-color: @color-gray-light;
-    border-radius: @inf;
-
-    box-shadow: -4px -4px 4px 0px inset #0002, 4px 4px 4px 0px inset #ffff;
-}
-</style>
