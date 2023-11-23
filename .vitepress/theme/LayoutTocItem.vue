aced71f49ca8cf562045759647ed9ed4c09f013e
MadCake <placeholder@email.com>
2023-11-23 09:12:01 +0100

    wip toc styling and skip single h1 page title

--- .vitepress/theme/LayoutTocItem.vue
+++ .vitepress/theme/LayoutTocItem.vue
@@ -6,11 +6,20 @@ const props = defineProps<{ heading: Heading }>();
 </script>
 
 <template>
-    <li>{{ props.heading.title }}</li>
-    <ul v-if="props.heading.children.length != 0">
-        <LayoutTocItem
-            v-for="child in props.heading.children"
-            :heading="child"
-        />
-    </ul>
+    <li :class="$style.TocItem">
+        <span>{{ props.heading.title }}</span>
+        <ul v-if="props.heading.children.length != 0">
+            <LayoutTocItem
+                v-for="child in props.heading.children"
+                :heading="child"
+            />
+        </ul>
+    </li>
 </template>
+
+<style module lang="less">
+.TocItem {
+    display: flex;
+    flex-direction: column;
+}
+</style>
