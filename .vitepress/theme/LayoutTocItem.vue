d22969525a246bd275ad87b3ce5c7c30986beb43
MadCake <placeholder@email.com>
2023-11-23 08:48:56 +0100

    wip toc

---
+++ .vitepress/theme/LayoutTocItem.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import LayoutToc from "./LayoutToc.vue";
+import { Heading } from "./LayoutToc.vue";
+
+const props = defineProps<{ heading: Heading }>();
+</script>
+
+<template>
+    <li>{{ props.heading.title }}</li>
+    <ul v-if="props.heading.children.length != 0">
+        <LayoutTocItem
+            v-for="child in props.heading.children"
+            :heading="child"
+        />
+    </ul>
+</template>
