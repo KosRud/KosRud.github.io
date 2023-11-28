257b23dc295e9580a2c61186b3d36bb070492e05
MadCake <placeholder@email.com>
2023-11-28 07:09:50 +0100

    refactor

--- .vitepress/theme/components/LayoutHeader.vue
+++ .vitepress/theme/components/LayoutHeader.vue
@@ -1,15 +1,12 @@
 <script lang="ts" setup>
+import { useData } from "vitepress";
 
-
-import { useData, useRoute } from "vitepress";
+import LayoutTopNav from "./LayoutTopNav.vue";
 
 import type { ThemeConfig } from "../ThemeConfig";
-import { urlMatch, UrlMatch } from "./composables/urlMatch";
 
 // https://vitepress.dev/reference/runtime-api#usedata
 const { site, } = useData<ThemeConfig>();
-const route = useRoute();
-
 
 </script>
 
@@ -23,20 +20,7 @@ const route = useRoute();
 					{{ site.description }}</span>
 			</div>
 		</a>
-		<nav :class="$style.TopNav">
-			<ul>
-				<li v-for="navItem in site.themeConfig.nav" :class="$style.NavItem">
-					<a :class="[
-						$style.NavItem_link,
-						[UrlMatch.full, UrlMatch.inside].includes(
-							urlMatch(route.path, navItem.url)
-						)
-							? $style.Header_navLink___active
-							: '',
-					]" :href="navItem.url">{{ navItem.title }}</a>
-				</li>
-			</ul>
-		</nav>
+		<LayoutTopNav />
 	</header>
 </template>
 
@@ -50,28 +34,6 @@ const route = useRoute();
 	gap: @gap;
 
 	height: @Header-height;
-}
-
-.TopNav {
-	>ul {
-		display: flex;
-		flex-direction: row;
-		height: 100%;
-		gap: @gap;
-		padding-top: @TopNav-gap-vertical;
-		padding-bottom: @TopNav-gap-vertical;
-	}
-}
-
-.NavItem {
-	&::before {
-		content: none;
-	}
-
-	display: flex;
-	flex-direction: column;
-	justify-content: stretch;
-	align-items: center;
 }
 
 .Header_navLink___active:link {
@@ -89,15 +51,8 @@ const route = useRoute();
 	}
 }
 
-.Header {
-	background-color: @color-background-dark;
-	display: flex;
-	gap: @gap;
 
-	height: @Header-height;
-}
-
-.Header_siteTitleContainer,
+.SiteTitleContainer,
 .SiteTitleContainer:link,
 .SiteTitleContainer:visited {
 	display: flex;
@@ -109,13 +64,6 @@ const route = useRoute();
 	line-height: 1;
 	position: relative;
 	text-decoration-line: inherit;
-
-	>div {
-		width: max-content;
-		display: flex;
-		justify-content: center;
-		flex-direction: column;
-	}
 
 	&::before {
 		content: "";
@@ -131,57 +79,19 @@ const route = useRoute();
 }
 
 .SiteTitle {
+	width: max-content;
+	display: flex;
+	justify-content: center;
+	flex-direction: column;
 	text-align: center;
 }
 
 .SiteDescription {
-	text-align: right;
-}
-
-.TopNav {
-	>ul {
-		display: flex;
-		flex-direction: row;
-		height: 100%;
-		gap: @gap;
-		padding-top: @TopNav-gap-vertical;
-		padding-bottom: @TopNav-gap-vertical;
-	}
-}
-
-.NavItem_link {
-	@navlink-pad: @gap*0.5;
-
-	flex: 1 1 calc(@navlink-pad*2 + 1em);
-
-	&:link,
-	&:visited {
-		color: @color-white;
-		text-decoration: inherit;
-	}
-
-	background: @color-primary;
-	background-clip: border-box;
-	border: @width solid #fff1;
-	border-radius: @gap*0.5;
-	line-height: 1;
-	padding: @navlink-pad;
-	min-width: @gap*11;
+	width: max-content;
 	display: flex;
-	align-items: center;
 	justify-content: center;
-	box-shadow: @shadow;
-	transition: background-color 0.2s;
-	transition: border-color 0.5s;
-
-	&:hover {
-		background-color: @color-primary-light;
-		border-color: @color-highlight;
-	}
-
-	&:active {
-		transform: translateY(@click-offset);
-	}
+	flex-direction: column;
+	text-align: right;
 }
 
 /*
