6ac05988467e53d73ccf3806e669c94bc927e579
MadCake <placeholder@email.com>
2023-11-30 16:27:29 +0100

    wip burger

---
+++ kosrud.github.io/.vitepress/theme/components/SiteTitle.vue
@@ -0,0 +1,90 @@
+<script setup lang="ts">
+import { useData } from "vitepress";
+
+import type { ThemeConfig } from "../ThemeConfig";
+
+// https://vitepress.dev/reference/runtime-api#usedata
+const { site } = useData<ThemeConfig>();
+</script>
+
+<template>
+    <a
+        :class="$style.SiteTitleContainer"
+        href="/"
+    >
+        <div>
+            <span :class="$style.SiteTitle"> {{ site.title }}</span>
+            <span :class="$style.SiteDescription"> {{ site.description }}</span>
+        </div>
+    </a>
+</template>
+
+<style lang="less" module>
+@import "../style/variables/index.less";
+
+.SiteTitleContainer,
+.SiteTitleContainer:link,
+.SiteTitleContainer:visited {
+    display: flex;
+    flex-direction: row;
+    justify-content: start;
+    align-items: center;
+    padding: 0rem @gap;
+
+    color: @color-white;
+    line-height: 1;
+    text-decoration-line: inherit;
+
+    &::before {
+        content: "";
+        height: 1.5em;
+        width: 1.5em;
+        background-image: url("/assets/icons/home/icon.svg");
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center;
+        margin-right: @gap;
+        margin-left: 0rem;
+    }
+
+    &:hover::before {
+        transform: scale(1.3);
+    }
+}
+
+.SiteTitle {
+    width: max-content;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    text-align: center;
+}
+
+.SiteDescription {
+    width: max-content;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    text-align: right;
+}
+
+/*
+	Font-size
+\*----------------------------------*/
+
+.SiteTitle {
+    font-size: @size-xl;
+}
+
+.SiteDescription {
+    font-size: @size-s;
+}
+
+/*
+	Font-family
+\*----------------------------------*/
+
+.SiteTitleContainer {
+    font-family: @font-hero;
+}
+</style>
