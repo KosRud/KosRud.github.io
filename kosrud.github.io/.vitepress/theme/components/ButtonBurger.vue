c281ca98b1efcf9dc4dca90455390d7da18e94d5
MadCake <placeholder@email.com>
2023-12-04 15:11:29 +0100

    working on burger menu

--- kosrud.github.io/.vitepress/theme/components/ButtonBurger.vue
+++ kosrud.github.io/.vitepress/theme/components/ButtonBurger.vue
@@ -4,10 +4,11 @@
 
 <template>
     <div :class="$style.BurgerWrapper">
+        <div :class="$style.Burger_title">Menu</div>
         <div :class="$style.Burger">
             <div
                 :class="$style.Burger_line"
-                v-for="_ in 3"
+                v-for="_ in 5"
             ></div>
         </div>
     </div>
@@ -17,11 +18,14 @@
 @import "../style/variables/index.less";
 
 .BurgerWrapper {
-    aspect-ratio: 1;
-
     display: flex;
     justify-content: stretch;
     align-items: stretch;
+    flex-direction: row;
+    color: @color-white;
+
+    padding-left: @gap;
+    padding-right: @gap;
 
     &:hover {
         background-color: @color-highlight;
@@ -34,24 +38,38 @@
             transform: translateY(@click-offset);
         }
     }
+
+    font-family: @font-ui;
+}
+
+.Burger_title {
+    display: flex;
+    align-items: center;
+    justify-content: center;
 }
 
 .Burger {
-    flex: 1 1;
+    aspect-ratio: 1;
+    flex: 1 1 0rem;
 
+    // background-color: red;
+
+    padding: @gap*0.5;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: stretch;
-    margin: 25% 15%;
-    gap: 30%;
+    gap: @border-width*2;
 }
 
 .Burger_line {
-    flex: 0 0 15%;
+    flex: 0 0 @border-width;
     border-radius: @inf;
     background-color: @color-background;
     box-shadow: @shadow-s;
+    &:nth-child(2n + 1) {
+        margin-right: 15%;
+    }
 }
 
 /*
