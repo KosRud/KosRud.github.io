808cb7807793df84908a50a94a6ae62d81e0e3cc
MadCake <placeholder@email.com>
2023-12-09 03:26:28 +0100

    styling homepage

--- kosrud.github.io/pages/components/HomePage_Bio.vue
+++ kosrud.github.io/pages/components/HomePage_Bio.vue
@@ -1,16 +1,13 @@
 <script setup lang="ts"></script>
 
 <template>
-    <div>
+    <div :class="$style.Bio">
         <h2>Greetings!</h2>
         <p>
             Name's Kostiantyn, I'm a programmer from Ukraine currently residing
-            in Bratislava, Slovakia.
-        </p>
-        <p>
-            This website contains an overview of my work (<a href="/projects/"
-                >projects</a
-            >, <a href="/papers/">papers</a>), as well as an assortment of
+            in Bratislava, Slovakia. This website contains an overview of my
+            work (<a href="/projects/">projects</a>,
+            <a href="/papers/">papers</a>), as well as an assortment of
             condensed <a href="#">highlights</a> from the web development
             seminars I've been teaching at
             <a href="https://www.fiit.stuba.sk/en.html"
@@ -22,3 +19,26 @@
         </p>
     </div>
 </template>
+
+<style module lang="less">
+@import "../../.vitepress/theme/style/variables/index.less";
+@import "../../.vitepress/theme/style/mixins/index.less";
+
+.Bio {
+    .Content();
+
+    font-size: @size-l;
+
+    h1,
+    h2,
+    h3,
+    h4,
+    p {
+        margin-bottom: @content-gap*1.5;
+    }
+
+    h2 {
+        font-size: @size-xxl;
+    }
+}
+</style>
