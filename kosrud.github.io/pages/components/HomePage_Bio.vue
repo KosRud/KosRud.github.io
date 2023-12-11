80c131081e0b24b611e52cab7eba0d026efd4dc9
MadCake <placeholder@email.com>
2023-12-11 07:12:21 +0100

    cypress icon, in-text adaptive bio photo

--- kosrud.github.io/pages/components/HomePage_Bio.vue
+++ kosrud.github.io/pages/components/HomePage_Bio.vue
@@ -8,125 +8,115 @@ const isNarrowThresholdsRem = [1100, 900, 700];
 const containerElement: Ref<Element | null> = ref(null);
 
 useResizeObserver(
-    () => {
-        if (!containerElement.value) {
-            console.error("Homepage bio container element not found");
-            return;
-        }
+	() => {
+		if (!containerElement.value) {
+			console.error("Homepage bio container element not found");
+			return;
+		}
 
-        const width = pxToRem(containerElement.value.clientWidth);
+		const width = pxToRem(containerElement.value.clientWidth);
 
-        adaptiveStage.value = -1;
-        isNarrowThresholdsRem.forEach((threshold, id) => {
-            if (width < threshold) {
-                adaptiveStage.value = id;
-            }
-        });
-    },
-    () => containerElement.value,
-    true
+		adaptiveStage.value = -1;
+		isNarrowThresholdsRem.forEach((threshold, id) => {
+			if (width < threshold) {
+				adaptiveStage.value = id;
+			}
+		});
+	},
+	() => containerElement.value,
+	true
 );
 
 const devIcons = {
-    worked: [
-        { url: "/assets/icons/dev/unity/icon.svg" },
-        { url: "/assets/icons/dev/csharp/icon.svg" },
-        { url: "/assets/icons/dev/hlsl/icon.svg" },
+	worked: [
+		{ url: "/assets/icons/dev/unity/icon.svg" },
+		{ url: "/assets/icons/dev/csharp/icon.svg" },
+		{ url: "/assets/icons/dev/hlsl/icon.svg" },
 
-        { url: "/assets/icons/dev/vue/icon.svg" },
-        { url: "/assets/icons/dev/pinia/icon.svg" },
+		{ url: "/assets/icons/dev/vue/icon.svg" },
+		{ url: "/assets/icons/dev/pinia/icon.svg" },
 
-        { url: "/assets/icons/dev/javascript/icon.svg" },
-        { url: "/assets/icons/dev/typescript/icon.svg" },
+		{ url: "/assets/icons/dev/javascript/icon.svg" },
+		{ url: "/assets/icons/dev/typescript/icon.svg" },
 
-        { url: "/assets/icons/dev/html5/icon.svg" },
-        { url: "/assets/icons/dev/css3/icon.svg" },
-        { url: "/assets/icons/dev/less/icon.svg" },
+		{ url: "/assets/icons/dev/html5/icon.svg" },
+		{ url: "/assets/icons/dev/css3/icon.svg" },
+		{ url: "/assets/icons/dev/less/icon.svg" },
 
-        { url: "/assets/icons/dev/immutable/icon.svg" },
-        { url: "/assets/icons/dev/typedoc/icon.png" },
-        { url: "/assets/icons/dev/node/icon.svg" },
+		{ url: "/assets/icons/dev/immutable/icon.svg" },
+		{ url: "/assets/icons/dev/typedoc/icon.png" },
+		{ url: "/assets/icons/dev/node/icon.svg" },
 
-        { url: "/assets/icons/dev/python/icon.svg" },
+		{ url: "/assets/icons/dev/python/icon.svg" },
 
-        { url: "/assets/icons/dev/cpp/icon.svg" },
+		{ url: "/assets/icons/dev/cpp/icon.svg" },
 
-        { url: "/assets/icons/dev/doxygen/icon.png" },
-        { url: "/assets/icons/dev/git/icon.svg" },
-    ],
-    dabbled: [
-        { url: "/assets/icons/dev/react/icon.svg" },
-        { url: "/assets/icons/dev/redis/icon.svg" },
-        { url: "/assets/icons/dev/postgresql/icon.svg" },
-        { url: "/assets/icons/dev/mongodb/icon.svg" },
-        { url: "/assets/icons/dev/fastify/icon.svg" },
-        { url: "/assets/icons/dev/deno/icon.svg" },
-        { url: "/assets/icons/dev/cython/icon.svg" },
-        { url: "/assets/icons/dev/lua/icon.svg" },
-        { url: "/assets/icons/dev/rust/icon.svg" },
-    ],
+		{ url: "/assets/icons/dev/doxygen/icon.png" },
+		{ url: "/assets/icons/dev/git/icon.svg" },
+	],
+	dabbled: [
+		{ url: "/assets/icons/dev/react/icon.svg" },
+		{ url: "/assets/icons/dev/redis/icon.svg" },
+		{ url: "/assets/icons/dev/postgresql/icon.svg" },
+		{ url: "/assets/icons/dev/mongodb/icon.svg" },
+		{ url: "/assets/icons/dev/cypress/icon.svg" },
+		{ url: "/assets/icons/dev/fastify/icon.svg" },
+		{ url: "/assets/icons/dev/deno/icon.svg" },
+		{ url: "/assets/icons/dev/cython/icon.svg" },
+		{ url: "/assets/icons/dev/lua/icon.svg" },
+		{ url: "/assets/icons/dev/rust/icon.svg" },
+	],
 };
 </script>
 
 <template>
-    <div
-        :class="{
-            [$style.Bio___adaptive_0]: adaptiveStage >= 0,
-            [$style.Bio___adaptive_1]: adaptiveStage >= 1,
-            [$style.Bio___adaptive_2]: adaptiveStage >= 2,
-        }"
-        :ref="(element) => {containerElement = element as Element}"
-    >
-        <div :class="$style.Bio_titleContainer">
-            <div :class="$style.Bio_titleSpacer"></div>
-            <h2 :class="$style.Bio_title">Greetings!</h2>
-        </div>
+	<div :class="{
+		[$style.Bio___adaptive_0]: adaptiveStage >= 0,
+		[$style.Bio___adaptive_1]: adaptiveStage >= 1,
+		[$style.Bio___adaptive_2]: adaptiveStage >= 2,
+	}" :ref="(element) => { containerElement = element as Element }">
+		<div :class="$style.Bio_titleContainer">
+			<div :class="$style.Bio_titleSpacer"></div>
+			<h2 :class="$style.Bio_title">Greetings!</h2>
+		</div>
 
-        <div :class="$style.Bio_content">
-            <img
-                :class="$style.Bio_photo"
-                src="/assets/photo.png"
-            />
-            <div :class="[$style.Bio_text]">
-                <p>
-                    Name's Kostiantyn. I'm a programmer from Ukraine currently
-                    residing in Bratislava, Slovakia. This website contains an
-                    overview of my work (<a href="/projects/">projects</a>,
-                    <a href="/papers/">papers</a>), as well as an assortment of
-                    condensed <a href="#">highlights</a> from the web
-                    development seminars I've been teaching at
-                    <a href="https://www.fiit.stuba.sk/en.html"
-                        ><span
-                            title="Faculty of Informatics and Information Technologies, Slovak University of Technology"
-                            >FIIT STU</span
-                        ></a
-                    >.
-                </p>
-                <table :class="$style.DeviconsTable">
-                    <tr>
-                        <td>I have experience working with:</td>
-                        <td>
-                            <img
-                                :class="$style.devIcon"
-                                v-for="devIcon in devIcons.worked"
-                                :src="devIcon.url"
-                            />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>I have dabbled with:</td>
-                        <td>
-                            <img
-                                :class="$style.devIcon"
-                                :src="devIcon.url"
-                                v-for="devIcon in devIcons.dabbled"
-                            />
-                        </td>
-                    </tr>
-                </table>
-            </div>
-        </div>
-    </div>
+		<div :class="$style.Bio_content">
+			<img :class="[$style.Bio_photo, $style.Bio_photo___aside]" src="/assets/photo.png" v-if="adaptiveStage < 2" />
+			<div :class="[$style.Bio_text]">
+				<p>
+					Name's Kostiantyn. I'm a programmer from Ukraine currently
+					residing in Bratislava, Slovakia. This website contains an
+					overview of my work (<a href="/projects/">projects</a>,
+					<a href="/papers/">papers</a>), as well as an assortment of
+					condensed <a href="#">highlights</a> from the web
+					development seminars I've been teaching at
+					<a href="https://www.fiit.stuba.sk/en.html"><span
+							  title="Faculty of Informatics and Information Technologies, Slovak University of Technology">FIIT
+							STU</span></a>.
+				</p>
+				<p>
+					<img :class="[$style.Bio_photo, $style.Bio_photo___inside]" src="/assets/photo.png"
+						 v-if="adaptiveStage >= 2" />
+				</p>
+				<p>
+				<table :class="$style.DeviconsTable">
+					<tr>
+						<td>I have experience working with:</td>
+						<td>
+							<img :class="$style.devIcon" v-for="devIcon in devIcons.worked" :src="devIcon.url" />
+						</td>
+					</tr>
+					<tr>
+						<td>I have dabbled with:</td>
+						<td>
+							<img :class="$style.devIcon" :src="devIcon.url" v-for="devIcon in devIcons.dabbled" />
+						</td>
+					</tr>
+				</table>
+				</p>
+			</div>
+		</div>
+	</div>
 </template>
 
 <style module lang="less">
@@ -138,87 +128,101 @@ const devIcons = {
 @Bio_content-gap: @gap*1.5;
 
 .DeviconsTable {
-    td:first-child {
-        text-align: right;
-        padding-right: @gap*1.5;
-        vertical-align: top;
-        white-space: nowrap;
-    }
+	td:first-child {
+		text-align: right;
+		padding-right: @gap*1.5;
+		vertical-align: top;
+		white-space: nowrap;
+	}
 
-    td {
-        padding-bottom: @gap*0.5;
-        padding-top: @gap*0.5;
-    }
+	td {
+		padding-bottom: @gap*0.5;
+		padding-top: @gap*0.5;
+	}
 }
 
 .Bio_title {
-    flex: 1 1 600rem;
+	flex: 1 1 600rem;
 
-    font-size: @size-xl;
-    font-weight: bold;
-    max-width: @content-width;
-    margin-bottom: @Bio_content-gap;
+	font-size: @size-xl;
+	font-weight: bold;
+	max-width: @content-width;
+	margin-bottom: @Bio_content-gap;
 }
 
 .Bio_content {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap-reverse;
-    gap: @Bio_content-gap @Bio_photo-to-content-gap;
-    justify-content: center;
-    align-items: start;
+	display: flex;
+	flex-direction: row;
+	flex-wrap: wrap-reverse;
+	gap: @Bio_content-gap @Bio_photo-to-content-gap;
+	justify-content: center;
+	align-items: start;
 
-    margin-right: @Bio_photo-width + @Bio_photo-to-content-gap;
+	margin-right: @Bio_photo-width + @Bio_photo-to-content-gap;
 }
 
 .Bio_text {
-    .Content();
-    flex: 1 1 400rem;
+	.Content();
+	flex: 1 1 400rem;
 
-    font-size: @size-l;
-    text-align: justify;
+	font-size: @size-l;
+	text-align: justify;
 
-    h1,
-    h2,
-    h3,
-    h4,
-    p {
-        margin-bottom: @Bio_content-gap;
-    }
+	h1,
+	h2,
+	h3,
+	h4,
+	p {
+		margin-bottom: @Bio_content-gap;
+	}
 }
 
 .Bio_titleContainer {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    margin-right: @Bio_photo-width + @Bio_photo-to-content-gap;
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: center;
+	margin-right: @Bio_photo-width + @Bio_photo-to-content-gap;
 }
 
 .Bio_titleSpacer {
-    flex: 0 0 @Bio_photo-width + @Bio_photo-to-content-gap;
-    height: 0rem;
+	flex: 0 0 @Bio_photo-width + @Bio_photo-to-content-gap;
+	height: 0rem;
 }
 
 .Bio_photo {
-    flex: 0 1 @Bio_photo-width;
-    border-radius: 50% @gap @gap 30% / 50% @gap @gap 50%;
-    box-shadow: @shadow;
+	box-shadow: @shadow;
+	object-fit: cover;
+}
 
-    object-fit: cover;
+.Bio_photo___aside {
+	flex: 0 1 @Bio_photo-width;
+	border-radius: 50% @gap @gap 30% / 50% @gap @gap 50%;
 
-    margin-top: @size-l*0.3;
-    margin-bottom: @gap;
+
+	margin-top: @size-l*0.3;
+	margin-bottom: @gap;
+}
+
+.Bio_photo___inside {
+	border-radius: @gap @gap 30% 30% / @gap @gap 50% 50%;
+
+	height: 160rem;
+
+	margin-left: auto;
+	margin-right: auto;
+	display: block;
 }
 
 .devIcon {
-    height: @size*2;
-    vertical-align: middle;
+	height: @size*2;
+	vertical-align: middle;
 
-    &:first-child {
-        margin-left: @gap*0.5;
-    }
-    margin: @gap*0.25;
-    transform: translateY(-15%);
+	&:first-child {
+		margin-left: @gap*0.5;
+	}
+
+	margin: @gap*0.25;
+	transform: translateY(-15%);
 }
 
 /*
@@ -226,61 +230,60 @@ const devIcons = {
 \*----------------------------------*/
 
 .Bio___adaptive_0 {
-    .Bio_content,
-    .Bio_titleContainer {
-        margin-right: 0rem;
-    }
+
+	.Bio_content,
+	.Bio_titleContainer {
+		margin-right: 0rem;
+	}
 }
 
 .Bio___adaptive_1 {
-    .Bio_content {
-        gap: @gap*2;
-    }
+	.Bio_content {
+		gap: @gap*2;
+	}
 
-    .DeviconsTable {
-        td {
-            white-space: wrap;
+	.DeviconsTable {
+		td {
+			white-space: wrap;
 
-            &:first-child {
-                width: 100%;
-                text-align: left;
-            }
-        }
+			&:first-child {
+				width: 100%;
+				text-align: left;
+			}
+		}
 
-        tr {
-            display: flex;
-            flex-wrap: wrap;
-        }
-    }
+		tr {
+			display: flex;
+			flex-wrap: wrap;
+		}
+	}
 
-    .Bio_titleSpacer {
-        display: none;
-    }
+	.Bio_titleSpacer {
+		display: none;
+	}
 
-    .Bio_title {
-        text-align: center;
-    }
+	.Bio_title {
+		text-align: center;
+	}
 }
 
 .Bio___adaptive_2 {
-    .Bio_text {
-        flex: 1 0 100%;
-        text-align: left;
-    }
+	.Bio_text {
+		flex: 1 0 100%;
+		text-align: left;
+	}
 
-    .Bio_photo {
-        border-radius: @gap @gap 30% 30% / @gap @gap 50% 50%;
-    }
+
 }
 
 .Bio___narrow {
-    .Bio_content {
-        gap: @gap*2 @gap*4;
-        margin-right: 0rem;
-    }
+	.Bio_content {
+		gap: @gap*2 @gap*4;
+		margin-right: 0rem;
+	}
 
-    .Bio_titleContainer {
-        margin-right: 0rem;
-    }
+	.Bio_titleContainer {
+		margin-right: 0rem;
+	}
 }
 </style>
