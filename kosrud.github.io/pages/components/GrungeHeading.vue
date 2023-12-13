355b6d8c043126ac17d0e5f209abac6bbe890f7f
MadCake <placeholder@email.com>
2023-12-13 06:57:16 +0100

    GrungeHeading

---
+++ kosrud.github.io/pages/components/GrungeHeading.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+const props = defineProps<{ text: string }>();
+</script>
+
+<template>
+    <h2 :class="$style.HeadingWrapper">
+        {{ props.text }}
+    </h2>
+</template>
+
+<style module lang="less">
+@import "../../.vitepress/theme/style/variables/index.less";
+@import "../../.vitepress/theme/style/mixins/index.less";
+
+.HeadingWrapper {
+    width: min(300rem, 100%);
+
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: @gap*4;
+
+    font-weight: bold;
+    color: @color-white;
+    font-size: @size-xxl;
+
+    text-align: center;
+    white-space: nowrap;
+
+    background-image: url("/assets/smudges/2.svg");
+    background-size: 100% 100%;
+    padding: 5rem 50rem 21rem 40rem;
+    transform: translateX(5rem);
+    background-repeat: no-repeat;
+    background-position: center;
+
+    text-shadow: 3rem 1rem 0rem black;
+
+    position: relative;
+}
+</style>
