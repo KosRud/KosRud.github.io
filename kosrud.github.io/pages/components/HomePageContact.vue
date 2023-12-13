03e1bf98d9c7d2748990807b13004f6e854bbfc1
MadCake <placeholder@email.com>
2023-12-13 07:16:26 +0100

    todo links

--- kosrud.github.io/pages/components/HomePageContact.vue
+++ kosrud.github.io/pages/components/HomePageContact.vue
@@ -9,4 +9,10 @@ import GrungeHeading from "./GrungeHeading.vue";
         href="https://medium.com/@naomiHauret/creating-a-contact-form-with-no-back-end-5248fb6bf7f7"
         >ToDo</a
     >
+    <br />
+    <a href="https://www.jotform.com/form-templates/category/contact-form"
+        >Templates</a
+    >
+    <br />
+    <a href="https://formspree.io/">Formspree</a>
 </template>
