f743473999f7f8fd0782553cdee5a879c46ecff5
MadCake <placeholder@email.com>
2023-12-15 03:25:13 +0100

    fixed highlights grid

--- kosrud.github.io/pages/components/HomePageHighlights.vue
+++ kosrud.github.io/pages/components/HomePageHighlights.vue
@@ -6,70 +6,83 @@ const props = defineProps<{ dummies: number }>();
 </script>
 
 <template>
-    <GrungeHeading text="My projects" />
-    <div :class="$style.ProjectsContainer">
-        <HomePageHighlightsProject :title="'DQ skinning for Unity'">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac
-            dignissim nibh. Sed pharetra sem et congue faucibus. Maecenas eget
-            libero lacus. Nunc non eros id risus accumsan ullamcorper. Nullam
-            sit amet purus risus. Aliquam erat volutpat. Vivamus et lacus
-            posuere, rutrum magna quis, faucibus nunc.
-        </HomePageHighlightsProject>
-        <HomePageHighlightsProject :title="'dice-pool-calc'">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac
-            dignissim nibh. Sed pharetra sem et congue faucibus. Maecenas eget
-            libero lacus. Nunc non eros id risus accumsan ullamcorper. Nullam
-            sit amet purus risus. Aliquam erat volutpat. Vivamus et lacus
-            posuere, rutrum magna quis, faucibus nunc.
-        </HomePageHighlightsProject>
-        <HomePageHighlightsProject :title="'This website'">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac
-            dignissim nibh. Sed pharetra sem et congue faucibus. Maecenas eget
-            libero lacus. Nunc non eros id risus accumsan ullamcorper. Nullam
-            sit amet purus risus. Aliquam erat volutpat. Vivamus et lacus
-            posuere, rutrum magna quis, faucibus nunc.
-        </HomePageHighlightsProject>
-        <HomePageHighlightsProject :title="'joplin-plugin-paginator'">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac
-            dignissim nibh. Sed pharetra sem et congue faucibus. Maecenas eget
-            libero lacus. Nunc non eros id risus accumsan ullamcorper. Nullam
-            sit amet purus risus. Aliquam erat volutpat. Vivamus et lacus
-            posuere, rutrum magna quis, faucibus nunc.
-        </HomePageHighlightsProject>
-        <HomePageHighlightsProject :title="'Material Clustering Benchmark'">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac
-            dignissim nibh. Sed pharetra sem et congue faucibus. Maecenas eget
-            libero lacus. Nunc non eros id risus accumsan ullamcorper. Nullam
-            sit amet purus risus. Aliquam erat volutpat. Vivamus et lacus
-            posuere, rutrum magna quis, faucibus nunc.
-        </HomePageHighlightsProject>
-        <HomePageHighlightsProject
-            :title="'ProTeGe v2'"
-            :images="[
-                '/content/project screenshots/Protege v2/1.png',
-                '/content/project screenshots/Protege v2/3.png',
-            ]"
-        >
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac
-            dignissim nibh. Sed pharetra sem et congue faucibus. Maecenas eget
-            libero lacus. Nunc non eros id risus accumsan ullamcorper. Nullam
-            sit amet purus risus. Aliquam erat volutpat. Vivamus et lacus
-            posuere, rutrum magna quis, faucibus nunc.
-        </HomePageHighlightsProject>
-        <HomePageHighlightsProject
-            :title="'ProTeGe v1'"
-            :images="['/content/project screenshots/Protege v1/1.png']"
-        >
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac
-            dignissim nibh. Sed pharetra sem et congue faucibus. Maecenas eget
-            libero lacus. Nunc non eros id risus accumsan ullamcorper. Nullam
-            sit amet purus risus. Aliquam erat volutpat. Vivamus et lacus
-            posuere, rutrum magna quis, faucibus nunc.
-        </HomePageHighlightsProject>
-        <div
-            :class="$style.ProjectsContainer_dummy"
-            v-for="_ in Array.from({ length: props.dummies })"
-        ></div>
+    <div>
+        <GrungeHeading text="My projects" />
+        <div :class="$style.HighlightsWrapper">
+            <div :class="$style.Highlights">
+                <HomePageHighlightsProject :title="'DQ skinning for Unity'">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Donec ac dignissim nibh. Sed pharetra sem et congue
+                    faucibus. Maecenas eget libero lacus. Nunc non eros id risus
+                    accumsan ullamcorper. Nullam sit amet purus risus. Aliquam
+                    erat volutpat. Vivamus et lacus posuere, rutrum magna quis,
+                    faucibus nunc.
+                </HomePageHighlightsProject>
+                <HomePageHighlightsProject :title="'dice-pool-calc'">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Donec ac dignissim nibh. Sed pharetra sem et congue
+                    faucibus. Maecenas eget libero lacus. Nunc non eros id risus
+                    accumsan ullamcorper. Nullam sit amet purus risus. Aliquam
+                    erat volutpat. Vivamus et lacus posuere, rutrum magna quis,
+                    faucibus nunc.
+                </HomePageHighlightsProject>
+                <HomePageHighlightsProject :title="'This website'">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Donec ac dignissim nibh. Sed pharetra sem et congue
+                    faucibus. Maecenas eget libero lacus. Nunc non eros id risus
+                    accumsan ullamcorper. Nullam sit amet purus risus. Aliquam
+                    erat volutpat. Vivamus et lacus posuere, rutrum magna quis,
+                    faucibus nunc.
+                </HomePageHighlightsProject>
+                <HomePageHighlightsProject :title="'joplin-plugin-paginator'">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Donec ac dignissim nibh. Sed pharetra sem et congue
+                    faucibus. Maecenas eget libero lacus. Nunc non eros id risus
+                    accumsan ullamcorper. Nullam sit amet purus risus. Aliquam
+                    erat volutpat. Vivamus et lacus posuere, rutrum magna quis,
+                    faucibus nunc.
+                </HomePageHighlightsProject>
+                <HomePageHighlightsProject
+                    :title="'Material Clustering Benchmark'"
+                >
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Donec ac dignissim nibh. Sed pharetra sem et congue
+                    faucibus. Maecenas eget libero lacus. Nunc non eros id risus
+                    accumsan ullamcorper. Nullam sit amet purus risus. Aliquam
+                    erat volutpat. Vivamus et lacus posuere, rutrum magna quis,
+                    faucibus nunc.
+                </HomePageHighlightsProject>
+                <HomePageHighlightsProject
+                    :title="'ProTeGe v2'"
+                    :images="[
+                        '/content/project screenshots/Protege v2/1.png',
+                        '/content/project screenshots/Protege v2/3.png',
+                    ]"
+                >
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Donec ac dignissim nibh. Sed pharetra sem et congue
+                    faucibus. Maecenas eget libero lacus. Nunc non eros id risus
+                    accumsan ullamcorper. Nullam sit amet purus risus. Aliquam
+                    erat volutpat. Vivamus et lacus posuere, rutrum magna quis,
+                    faucibus nunc.
+                </HomePageHighlightsProject>
+                <HomePageHighlightsProject
+                    :title="'ProTeGe v1'"
+                    :images="['/content/project screenshots/Protege v1/1.png']"
+                >
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Donec ac dignissim nibh. Sed pharetra sem et congue
+                    faucibus. Maecenas eget libero lacus. Nunc non eros id risus
+                    accumsan ullamcorper. Nullam sit amet purus risus. Aliquam
+                    erat volutpat. Vivamus et lacus posuere, rutrum magna quis,
+                    faucibus nunc.
+                </HomePageHighlightsProject>
+                <div
+                    :class="$style.Highlights_dummy"
+                    v-for="_ in Array.from({ length: props.dummies })"
+                ></div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -77,35 +90,24 @@ const props = defineProps<{ dummies: number }>();
 @import "../../.vitepress/theme/style/variables/index.less";
 @import "../../.vitepress/theme/style/mixins/index.less";
 
-.Projects_heading {
-    font-weight: bold;
-    width: fit-content;
-    margin-left: auto;
-    margin-right: auto;
+@Highlights-gap: @gap*2;
 
-    font-size: @size-xxl;
-    margin-bottom: @gap*4;
+.Highlights {
+    margin: -@Highlights-gap*0.5;
 
-    background-image: url("/assets/smudges/2.svg");
-    background-size: 100% 100%;
-    padding: 5rem 50rem 21rem 40rem;
-    transform: translateX(5rem);
-    background-repeat: no-repeat;
-    background-position: center;
-
-    color: @color-white;
-
-    text-shadow: 3rem 1rem 0rem black;
-}
-
-.ProjectsContainer {
     display: flex;
     flex-wrap: wrap;
     flex-direction: row;
-    gap: @gap*2;
 
     > * {
         flex: 1 1 500rem;
+
+        margin: @gap @Highlights-gap * 0.5;
     }
+}
+
+.Highlights_dummy {
+    margin-top: 0rem;
+    margin-bottom: 0rem;
 }
 </style>
