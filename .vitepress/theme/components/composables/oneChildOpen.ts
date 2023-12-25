aa60f2bfa095e2b400a8241a50bc171d54eb0b8b
MadCake <placeholder@email.com>
2023-12-25 12:03:32 +0100

    refactor / generalize useOneChildOpen.ts

--- .vitepress/theme/components/composables/oneChildOpen.ts
+++ .vitepress/theme/components/composables/oneChildOpen.ts
@@ -1,21 +1,9 @@
 import { ref, Ref } from 'vue';
 
-import { NavItem } from '@theme/ThemeConfig';
-import { urlMatch } from './urlMatch';
-import { useRoute } from 'vitepress';
+export function useOneChildOpen<T>(children: T[]) {
+	const idOpenChild: Ref<T | null> = ref(null);
 
-export function useOneChildOpen(navItems: NavItem[]) {
-	const route = useRoute();
-
-	const idOpenChild: Ref<number | null> = ref(null);
-
-	navItems.forEach((navItem, id) => {
-		if (urlMatch(route.path, navItem.url).inside) {
-			idOpenChild.value = id;
-		}
-	});
-
-	function toggleChild(id: number) {
+	function toggleChild(id: T) {
 		if (idOpenChild.value == id) {
 			idOpenChild.value = null;
 			return;
@@ -23,7 +11,7 @@ export function useOneChildOpen(navItems: NavItem[]) {
 		idOpenChild.value = id;
 	}
 
-	function isChildOpen(id: number) {
+	function isChildOpen(id: T) {
 		return idOpenChild.value == id;
 	}
 
