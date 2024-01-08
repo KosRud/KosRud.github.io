6a81e0f16d0631952e44060caacb3cf64a482be8
MadCake <placeholder@email.com>
2024-01-08 23:21:33 +0100

    restyle back to top

--- .vitepress/theme/components/BackTop.vue
+++ .vitepress/theme/components/BackTop.vue
@@ -1,13 +1,20 @@
 <script setup lang="ts">
 import { anchorIds } from './composables/anchorIds';
+import arrowCircled from './assets/icons/ui/arrowCircled/icon.svg';
 </script>
 
 <template>
-	<a
-		:class="$style.BackToTop"
-		:href="`#${anchorIds.page.root}`"
-		>Back to top</a
-	>
+	<div :class="$style.BackToTop">
+		<a
+			:class="$style.BackToTop_link"
+			:href="`#${anchorIds.page.root}`"
+			><img
+				alt="upward arrow"
+				:class="$style.Arrow"
+				:src="arrowCircled"
+			/>Back to top</a
+		>
+	</div>
 </template>
 
 <style module lang="less">
@@ -21,10 +28,6 @@ import { anchorIds } from './composables/anchorIds';
 	display: block;
 	width: min(@gap*32, 100%);
 
-	font-size: @size-l;
-	font-weight: bold;
-	font-family: @font-techy;
-
 	text-align: center;
 	padding: @gap;
 
@@ -33,5 +36,24 @@ import { anchorIds } from './composables/anchorIds';
 
 	background-color: @color-background;
 	box-shadow: @shadow-s;
+}
+
+.BackToTop_link {
+	font-size: @size-l;
+	font-weight: bold;
+	font-family: @font-techy;
+	text-decoration: none;
+
+	&:link,
+	&:visited {
+		color: @color-text-faded-1;
+	}
+}
+
+.Arrow {
+	width: 2em;
+	vertical-align: middle;
+	margin-right: @gap*0.5;
+	margin-left: @gap*0.5;
 }
 </style>
