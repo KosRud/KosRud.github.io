941ee9177f8b95c0f32169e542eeeb566564f2ef
MadCake <placeholder@email.com>
2024-09-22 21:14:39 +0200

    removed Dig util

--- .vitepress/theme/components/composables/tsUtil.ts
+++ .vitepress/theme/components/composables/tsUtil.ts
@@ -1,9 +1,2 @@
-export type Dig<T, K extends string | number | symbol> = T extends Record<
-	K,
-	infer U
->
-	? U
-	: never;
-
 export type Enum = { [key: string]: number };
 export type EnumValues<T extends Enum> = T[keyof T];
